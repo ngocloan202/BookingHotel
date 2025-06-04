9a103eed170ea2dd8ea68a00cb60c522a991cad3
ngocloan202 <chauthingocloan0309@gmail.com>
2025-06-04 16:07:02 +0700

    Update MongoDB connection URI and change test script to dev in package.json

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ var roomRouter = require('./routers/room');
 const contactRouter = require('./routers/contact');
 app.use(express.static('public'));
 
-var uri = 'mongodb+srv://oanhdth225720:%23oanh%23%2A%2A%2A@cluster0.ct8fl.mongodb.net/trangtin'; 
+var uri = 'mongodb+srv://loandth225685:Ngocloan2512@cluster0.orj8j.mongodb.net/hotel'; 
 mongoose.connect(uri).catch(err => console.log(err)); 
 
 app.set('views', './views'); 
 app.set('view engine', 'ejs'); 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter); 
 app.use('/room', roomRouter);
