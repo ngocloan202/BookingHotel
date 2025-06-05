00819d96a623e07d61e9dde107ad2a7fd4111a2e
MaiHuong64 <rain06042004@gmail.com>
2025-06-06 00:18:44 +0700

    Fetch and render room data in the room view

--- routers/room.js
+++ routers/room.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var room = require('../models/room');
 
 // GET /room/
 router.get('/', async (req, res) => {
-  res.render('room', { title: 'Danh sách phòng' });
+  var r = await room.find();
+  res.render('room', { title: 'Danh sách phòng',  room: r });
 });
 
 module.exports = router;
