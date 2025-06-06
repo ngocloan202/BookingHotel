82fc29b900f27393367d1f4bd1c1868520778737
ngocloan202 <chauthingocloan0309@gmail.com>
2025-06-06 16:30:30 +0700

    Refactor index.js to improve session handling and restructure router imports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const mongoose = require('mongoose');
-const session = require('express-session');
 
 const app = express();
 
@@ -9,53 +9,61 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Static folder
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Session config
 app.use(session({
-  secret: 'super-secret-key',
-  resave: false,
-  saveUninitialized: true
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
 }));
 
 // Add session to res.locals
 app.use((req, res, next) => {
-  res.locals.session = req.session;
-  next();
+    res.locals.session = req.session;
+    next();
 });
 
-// Routes
-const roomRoutes = require('./routes/room');
-const authRoutes = require('./routes/auth');
+// Tải tất cả các model để MongoDB tạo collection tương ứng
+require('./models/bill');
+require('./models/booking');
+require('./models/customer');
+require('./models/equipment');
+require('./models/national');
+require('./models/review');
+require('./models/room');
+require('./models/room_equipment');
+require('./models/roomtype');
+require('./models/service');
+require('./models/serviceuse');
+require('./models/user');
+
+// Import routers
 const indexRouter = require('./routers/index');
+const roomRouter = require('./routers/room');
 const contactRouter = require('./routers/contact');
+const authRouter = require('./routes/auth');
 
-app.use('/room', roomRoutes);
-app.use('/', authRoutes);
+// Use routers
 app.use('/', indexRouter);
+app.use('/room', roomRouter);
 app.use('/contact', contactRouter);
-
-// Home page
-app.get('/', (req, res) => {
-    res.render('index', {
-        path: '/',
-        user: req.user
-    });
-});
+app.use('/', authRouter);
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://oanhdth225720:%23oanh%23%2A%2A%2A@cluster0.ct8fl.mongodb.net/hotel')
+const uri = 'mongodb+srv://oanhdth225720:%23oanh%23%2A%2A%2A@cluster0.ct8fl.mongodb.net/hotel';
+mongoose.connect(uri)
     .then(() => {
         console.log('Connected to MongoDB');
         // Start server
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+        app.listen(3000, () => {
+            console.log('Server is running at http://127.0.0.1:3000');
         });
     })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+    .catch(err => console.log(err));
