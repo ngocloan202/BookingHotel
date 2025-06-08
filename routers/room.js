b6215102f74a3f999168011ec09850d364d2db06
ngocloan202 <chauthingocloan0309@gmail.com>
2025-06-08 08:21:32 +0700

    Refactor room and room type models, update module exports, and add contact and bill data models

--- routers/room.js
+++ routers/room.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Room = mongoose.model('Room');
+//const Room = mongoose.model('Room');
+const Room = require('../models/room'); // 
 const RoomType = mongoose.model('RoomTypeModel');
 
 router.get('/', async (req, res) => {
